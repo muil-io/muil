1b0701476d312a873ddbee141a8b69e8017b8664
Shahaf Shaked <shahaf@live.com>
2020-12-13 20:08:04 +0200

    Update qs.ts

--- apps/api/src/shared/utils/qs.ts
+++ apps/api/src/shared/utils/qs.ts
@@ -1,18 +1,22 @@
+const keywords = {
+  true: true,
+  false: false,
+  null: null,
+  undefined,
+};
+
 export const decodeProps = (props) =>
   Object.entries(props).reduce((prev, [key, value]) => {
     let decodedValue = value;
-    if (/^(\d+|\d*\.\d+)$/.test(value as string)) {
-      decodedValue = parseFloat(value as string);
-    }
 
-    const keywords = {
-      true: true,
-      false: false,
-      null: null,
-      undefined,
-    };
-    if ((value as string) in keywords) {
-      decodedValue = keywords[value as string];
+    try {
+      decodedValue = JSON.parse((value as unknown) as string);
+    } catch {
+      if (/^(\d+|\d*\.\d+)$/.test(value as string)) {
+        decodedValue = parseFloat(value as string);
+      } else if ((value as string) in keywords) {
+        decodedValue = keywords[value as string];
+      }
     }
 
     return {
