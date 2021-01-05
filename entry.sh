1377dedc193d5021b169511e0f7415652454a4de
Shahaf Shaked <shahaf@live.com>
2021-01-05 13:57:49 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -8,7 +8,7 @@ if [ ! -f "/app/db/muil.db" ]; then
   sqlite3 ./db/muil.db "VACUUM;"
   npx prisma db push --preview-feature
 else
-  npx prisma migrate resolve --applied "20210105095759_base" --preview-feature
+  npx prisma migrate resolve --applied "20210105095759_base" --preview-feature || true
   npx prisma migrate deploy --preview-feature
 fi
 
