29c5a98e2d0e6aa1e064bc56afb68cf06c776013
Shahaf Shaked <shahaf@live.com>
2021-01-17 16:13:14 +0200

    Update Templates.js

--- apps/client/src/templates/components/Templates.js
+++ apps/client/src/templates/components/Templates.js
@@ -49,7 +49,7 @@ const Templates = () => {
     return (
       <TemplateView
         selectedTemplate={selectedTemplate}
-        baseTemplateUrl={`${process.env.BASE_URL}/templates/${selectedBranch}/${selectedTemplate.templateId}`}
+        baseTemplateUrl={`${process.env.BASE_URL}/api/templates/${selectedBranch}/${selectedTemplate.templateId}`}
         onExit={() => setSelectedTemplate()}
       />
     );
