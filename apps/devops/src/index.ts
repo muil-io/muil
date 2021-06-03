3ebd64fbb67b9c7e9822cb85b77772cf471f944b
Shahaf Shaked <shahafs@welldone-software.com>
2021-06-03 17:05:36 +0300

    Update index.ts

--- apps/devops/src/index.ts
+++ apps/devops/src/index.ts
@@ -1,11 +1,32 @@
+import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 import * as pulumi from '@pulumi/pulumi';
 import { v4 as uuid } from 'uuid';
 
-const listener = new awsx.elasticloadbalancingv2.NetworkListener('muil', { port: 443 });
-
+const templatesDirectory = '/app/templates';
+const dbDirectory = '/app/db';
 export const adminUserName = 'admin@muil.io';
 export const adminPassword = uuid().replace(/-/g, '');
+
+// Create network listener on port 443
+const listener = new awsx.elasticloadbalancingv2.NetworkListener('muil', { port: 443 });
+
+// Create a EFS volume for persistence
+const dbFilesystem = new aws.efs.FileSystem('muil-db-fs', {
+  tags: { Name: 'muil-templates-fs' },
+});
+const templatesFilesystem = new aws.efs.FileSystem('muil-templates-fs', {
+  tags: { Name: 'muil-templates-fs' },
+});
+
+// Create the EFS Configuration for connecting to Fargate
+const dbVolumeConfiguration: aws.types.input.ecs.TaskDefinitionVolumeEfsVolumeConfiguration = {
+  fileSystemId: dbFilesystem.id,
+};
+const templatesVolumeConfiguration: aws.types.input.ecs.TaskDefinitionVolumeEfsVolumeConfiguration =
+  {
+    fileSystemId: templatesFilesystem.id,
+  };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const service = new awsx.ecs.FargateService('muil', {
@@ -47,8 +68,28 @@ const service = new awsx.ecs.FargateService('muil', {
             value: adminPassword,
           },
         ],
+        mountPoints: [
+          {
+            sourceVolume: 'muil-db-volume',
+            containerPath: dbDirectory,
+          },
+          {
+            sourceVolume: 'muil-templates-volume',
+            containerPath: templatesDirectory,
+          },
+        ],
       },
     },
+    volumes: [
+      {
+        name: 'muil-db-volume',
+        efsVolumeConfiguration: dbVolumeConfiguration,
+      },
+      {
+        name: 'muil-templates-volume',
+        efsVolumeConfiguration: templatesVolumeConfiguration,
+      },
+    ],
   },
 });
 
